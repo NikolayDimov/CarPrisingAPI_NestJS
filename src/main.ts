c8c0a754567726eb6a6d45bd20eaa6be1941540c
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-01-09 17:37:15 +0200

    cookieSession add in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// import { CookieSession } from 'cookie-session';  // if import not work because tsconfig.json
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(cookieSession({
+        keys: ['superCookie']   // encript information that is stored inside the cookie
+    }))
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,16 +16,26 @@ import { UpdateuserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto) // no password return for user
 
 export class UsersController {
-    constructor(private userService: UsersService) {}
+    constructor(
+        private userService: UsersService,
+        private authService: AuthService
+        ) {}
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        this.userService.create(body.email, body.password);
+        // this.userService.create(body.email, body.password);  // creating User with no authentication
+        return this.authService.signup(body.email, body.password);  // creating User with authentication
+    }
+
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto) {
+        return this.authService.signin(body.email, body.password);  // login User with authentication
     }
 
     // @UseInterceptors(new SerializerInterceptor(UserDto)) // no password return with Get method
