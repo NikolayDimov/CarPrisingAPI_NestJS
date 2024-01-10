5096e29e2461bfb30e1237058c7a90154b77c46d
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-01-10 11:03:48 +0200

    report table has userId column

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,34 @@
+import { Expose, Transform } from 'class-transformer';
+import { User } from '../../users/user.entity';
+
+export class ReportDto {
+    @Expose()
+    id: number;
+
+    @Expose()
+    price: number;
+
+    @Expose()
+    year: number;
+
+    @Expose()
+    lng: number;
+
+    @Expose()
+    lat: number;
+
+    @Expose()
+    make: string;
+
+    @Expose()
+    model: string;
+
+    @Expose()
+    mileage: number;
+
+    @Transform(({ obj }) => obj.user.id)
+    @Expose()
+    userId: number;
+}
+
+// The @Expose() decorator is used to mark class properties that should be exposed during the transformation process. When transforming an object using class-transformer, only the properties marked with @Expose() will be included in the transformation result. This allows you to control which properties are included or excluded during the transformation.
